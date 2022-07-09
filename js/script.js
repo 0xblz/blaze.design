71826b6a5e5be2203a4022e99101282409e06075
Blaze <blazepollard@icloud.com>
2022-07-09 04:41:53 +0000

    added spline bg

---
+++ js/script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/BI-dv1Q91L18dBbL/scene.splinecode');
