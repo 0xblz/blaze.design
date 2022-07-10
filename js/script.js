b0a3f44c4972501c7aaa1b051c8542f22f222d33
Blaze <blazepollard@icloud.com>
2022-07-10 20:07:54 +0000

    added instafeed

--- js/script.js
+++
@@ -1,5 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/BI-dv1Q91L18dBbL/scene.splinecode');
