bd89c18c9543d2f9f8dbfe0d1ab5b3c6d37cebbb
Blaze <blazepollard@icloud.com>
2022-07-18 19:07:49 +0000

    adobe fonts

--- script.js
+++
@@ -1,6 +0,0 @@
-// spline
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/l0RykZgcJAm4bQzZ/scene.splinecode');
